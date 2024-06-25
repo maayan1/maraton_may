e1e3ed9fc333e089a4e8b7f49a9791bb21d3109c
Maayan Simkins <atia.maayan@gmail.com>
2024-06-25 22:00:25 +0300

    adding pic

--- src/NoBg.jsx
+++ src/NoBg.jsx
@@ -2,6 +2,7 @@ import './NoBg.css';
 
 import React, { useRef} from 'react';
 
+import imgToRemove from './assets/imgToRemove'
 
 function NoBg(props) {
     //props.comt_type
@@ -16,12 +17,14 @@ function NoBg(props) {
   return (
       <div className='no_bg_cont'>
                     {props.comt_type === 'no_bg'?
-                    <>
+                    <div>
                     <div className='no_bg_cont_text'> אל תשכח להוריד את הקבצים. הם ימחקו אוטומטית כשתצא מהדף </div>
                     <div className='bg_color' onClick={focusInput}>צבע רקע</div>
                     <input type="color" ref={inputElement} className='color_input'/> 
-                    </>
-                    : <></>}                         
+                    </div>
+                    : <></>}     
+
+                    <img src={imgToRemove} className='image' alt='front'/>                    
        </div>
   );
 }
