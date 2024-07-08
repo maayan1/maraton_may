2399b928486024f1f9e4071d0cb907e31c161964
Maayan Simkins <atia.maayan@gmail.com>
2024-07-08 11:25:09 +0300

    using server

--- src/Eula.jsx
+++ src/Eula.jsx
@@ -9,11 +9,12 @@ function Eula(props) {
 
   return (
     <>
-    <div className='overlay'> </div>
-    <div className='Eula_cont'>
-      <img src={close1} className='close' alt='close popup' onClick={()=>{props.close_popup_func(false)}} />             
-         <div className='eula_text'>fsfsddf</div>
-    </div>
+      <div className='overlay'> </div>
+      <div className='Eula_cont'>
+        <img src={close1} className='close' alt='close popup' onClick={()=>{props.close_popup_func(false)}} />             
+        
+        <div className='eula_text'>fsfsddf</div>
+      </div>
     </>
   );
 }
